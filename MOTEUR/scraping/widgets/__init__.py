9df872e5a8d840c77ddb69427c30eba814b9e999
razoraze123 <garbalamine@yahoo.com>
2025-07-18 22:46:16 +0200

    Add variant comparison widget

--- MOTEUR/scraping/widgets/__init__.py
+++ MOTEUR/scraping/widgets/__init__.py
@@ -2,4 +2,5 @@ from .scraping_widget import ScrapingImagesWidget
 from .profile_widget import ProfileWidget
 from .variant_widget import ScrapingVariantsWidget
 from .woo_url_widget import WooImageURLWidget
+from .variant_comparison_widget import VariantComparisonWidget
 from .scrap_widget import ScrapWidget
